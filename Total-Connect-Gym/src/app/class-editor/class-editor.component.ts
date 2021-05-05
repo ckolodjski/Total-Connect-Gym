7b6222eb187e7f9e8e2d2a9074dc61fc5cb354c1
etfargo <etfargo@hotmail.com>
2021-05-04 19:42:10 -0500

    Class Editor Layout and Basic func
    Basic functionality due to no dbservice yet. Uses alerts to show.
    
    Do we need to make Class class?

--- Total-Connect-Gym/src/app/class-editor/class-editor.component.ts
+++ Total-Connect-Gym/src/app/class-editor/class-editor.component.ts
@@ -7,8 +7,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ClassEditorComponent implements OnInit {
 
+  showAddCard: boolean = false;
+  showRemoveCard: boolean = false;
   constructor() { }
 
+  addButtonClick() { 
+    if (this.showRemoveCard){ this.showRemoveCard = false; }
+    this.showAddCard = !this.showAddCard;
+  }
+  removeButtonClick() { 
+    if (this.showAddCard){ this.showAddCard = false; }
+    this.showRemoveCard = !this.showRemoveCard;
+  }
+  //fetch class list, search
+  // string should be fine for searching
+  searchClass(c: string) {
+      alert("searched for " + c + "\n need to do db stuff here like the Tour of Heroes")
+  }
+  //add class to db
+  addClass(name: string, desc: string) {
+    alert("Class to add: " + "\n{\nName: " + name + "\nDesc: " + desc + "\n}")
+  }
+  //remove class
+  // need to make this input as a class???
+  removeClass(c: string) {
+    alert("Class to remove is " + c + "\n MAKE SURE YOU ARE SURE")
+    this.showRemoveCard = !this.showRemoveCard;
+    console.log("class removed: " + c);
+  }
   ngOnInit(): void {
   }
 
