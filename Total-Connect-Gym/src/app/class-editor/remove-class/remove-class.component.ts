0efc3053c4687b29e85e83417ef5d76d98255e47
etfargo <etfargo@hotmail.com>
2021-05-05 16:26:31 -0500

    OutputBinding to get functionality back

--- Total-Connect-Gym/src/app/class-editor/remove-class/remove-class.component.ts
+++ Total-Connect-Gym/src/app/class-editor/remove-class/remove-class.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-remove-class',
@@ -7,12 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RemoveClassComponent implements OnInit {
 
+  @Output() remove: EventEmitter<any> = new EventEmitter();
+
   constructor() { }
 
   removeClass(c: string) {
     alert("Class to remove is " + c + "\n MAKE SURE YOU ARE SURE")
     //this.showRemoveCard = !this.showRemoveCard;
     console.log("class removed: " + c);
+    this.remove.emit();
   }
 
   ngOnInit(): void {
