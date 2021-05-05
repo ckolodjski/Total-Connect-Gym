bba8a95f9b26193fbedb9591251e6a0597a6f9b9
Thomas Bye <ThomasBye1@outlook.com>
2021-05-05 14:44:13 +0000

    Added firefox debug config, changes to make firebase work

--- Total-Connect-Gym/src/app/data-types/course.ts
+++ Total-Connect-Gym/src/app/data-types/course.ts
@@ -1,17 +1,11 @@
 import { v4 as uuid } from 'uuid';
 
 //Represents a class offered by the gym.
-export class Course {
+export interface Course {
     Name: string;
     Description: string;
 
-    //The overall ID for a offered course. 
+    //The overall ID for a offered course. This should be a uuid, which can be set by calling uuid() from the uuid lib.
     //Not to be confused with the ClassID on the ScheduledGymClass as this represents the overall course while the ScheduledGymClass represents a specific instance of the course.
     CourseID: string;
-
-    constructor(name: string, description: string = "No class description was registered.") {
-        this.Name = name;
-        this.Description = description;
-        this.CourseID = uuid();
-    }
 }
--- Total-Connect-Gym/src/app/data-types/gym-class.ts
+++ Total-Connect-Gym/src/app/data-types/gym-class.ts
@@ -4,20 +4,13 @@ import { Course } from "./course";
 import { v4 as uuid } from 'uuid';
 
 //Represents a class that is on the gym's offered classes schedule.
-export class GymClass {
+export interface GymClass {
     ClassInformation: Course;
     Day: Day;
     StartTime: Date;
     EndTime: Date;
 
     //The unique ID for the class that represents a specific day and time of a course.
+    //This should be a uuid settable by calling uuid() from the uuid lib.
     ClassInstanceId: string;
-
-    constructor(classInformation: Course, day: Day, startTime: Date, endTime: Date) {
-        this.ClassInformation = classInformation;
-        this.Day = day;
-        this.StartTime = startTime;
-        this.EndTime = endTime;
-        this.ClassInstanceId = uuid();
-    }
 }
