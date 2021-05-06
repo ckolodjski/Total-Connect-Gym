1b98da96990ae0fe7910946413cfc2901a081ab7
Cameron Kolodjski <43521811+ckolodjski@users.noreply.github.com>
2021-05-06 04:39:59 -0500

    Add basic member check-in TS.

--- Total-Connect-Gym/src/app/member-check-in/member-check-in.component.ts
+++ Total-Connect-Gym/src/app/member-check-in/member-check-in.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { isSome, Option } from 'fp-ts/lib/Option';
+import { Course } from '../data-types/course';
+import { DatabaseService } from '../database.service';
 
 @Component({
   selector: 'app-member-check-in',
@@ -7,9 +10,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MemberCheckInComponent implements OnInit {
 
-  constructor() { }
+  private _dbService: DatabaseService;
+
+  constructor(dbService: DatabaseService) {
+    this._dbService = dbService;
+  }
 
   ngOnInit(): void {
+  }
+
+  async checkIn(memberID: string) {
+    // TODO call db service function, remove commented code if unnecessary
+    this._dbService.checkInMember(memberID);
+    /*
+    var res = await this._dbService.checkInMember(memberID);
+    if (isSome(res)) {
+      console.log(res);
+    } else {
+      console.log("Member check-in failed.");
+    }
+    */
   }
 
 }
