01b2740d4370edcbeecc2ab2d53ef25d4da10b99
etfargo <etfargo@hotmail.com>
2021-05-06 19:58:44 -0500

    added type list on mem reg

--- Total-Connect-Gym/src/app/member-registration/member-registration.component.ts
+++ Total-Connect-Gym/src/app/member-registration/member-registration.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { MembershipLevel } from '../data-types/membership';
 import { DatabaseService } from '../database.service';
 import { v4 as uuidv4 } from 'uuid';
+import { isSome } from 'fp-ts/lib/Option';
 @Component({
   selector: 'app-member-registration',
   templateUrl: './member-registration.component.html',
@@ -10,9 +11,22 @@ import { v4 as uuidv4 } from 'uuid';
 export class MemberRegistrationComponent implements OnInit {
 
   constructor(private dbService: DatabaseService) { }
-  
+  membershipTypes: MembershipLevel[] = [];
+
+  headers = ["Name:   ", "Price:   ", "ID:      "];
+  //fetch membership list and for populating the tapable
+  async fetchMembershipTypes() {
+    var res = await this.dbService.getMembershipLevels()
+
+    if (isSome(res)) {
+      this.membershipTypes = res.value;
+    }
+
+  }
+
 
   ngOnInit(): void {
+    this.fetchMembershipTypes();
   }
 
 }
--- Total-Connect-Gym/src/app/membership-editor/membership-editor.component.ts
+++ Total-Connect-Gym/src/app/membership-editor/membership-editor.component.ts
@@ -39,12 +39,6 @@ export class MembershipEditorComponent implements OnInit {
       this.membershipTypes = res.value;
     }
     
-    // const memtype: MembershipLevel = {Name: "Single Monthly", Price: 29.99, UniqueID: uuidv4()}
-    // const memtype2: MembershipLevel = {Name: "Couple Monthly", Price: 39.99, UniqueID: uuidv4() }
-    // this.membershipTypes = [memtype, memtype2]
-    for(var mem in this.membershipTypes) {
-      this.rows.push(mem)
-    }
   }
   
 
