40ed992a637c2e7e417fd3778e4a56b6acfadb03
etfargo <etfargo@hotmail.com>
2021-05-06 19:16:20 -0500

    Hope this isn't breaking

---
+++ Total-Connect-Gym/src/app/gymclass-editor/gymclass-editor.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-gymclass-editor',
+  templateUrl: './gymclass-editor.component.html',
+  styleUrls: ['./gymclass-editor.component.css']
+})
+export class GymclassEditorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
