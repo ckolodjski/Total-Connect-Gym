9aad8a477a015c250b73536026ba99a54cb93abd
WelliTried415 <jtinnd@live.com>
2021-05-07 07:49:40 -0500

    side nav css changes

--- Total-Connect-Gym/src/app/metrics/metrics.component.ts
+++ Total-Connect-Gym/src/app/metrics/metrics.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./metrics.component.css']
 })
 export class MetricsComponent implements OnInit {
-
+  
   constructor() { }
 
   ngOnInit(): void {
