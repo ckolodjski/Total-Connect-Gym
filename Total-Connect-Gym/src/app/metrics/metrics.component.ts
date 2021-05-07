bd26260d9390b60c0c1306b3ba5d411b252c7944
WelliTried415 <jtinnd@live.com>
2021-05-07 09:00:43 -0500

    metrics stuff

--- Total-Connect-Gym/src/app/metrics/metrics.component.ts
+++ Total-Connect-Gym/src/app/metrics/metrics.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DatabaseService } from '../database.service';
 
 @Component({
   selector: 'app-metrics',
@@ -6,11 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./metrics.component.css']
 })
 export class MetricsComponent implements OnInit {
-  capacity = "100";
-  revenue = "$10";
-  constructor() { }
+  capacity;
+  revenue;
+  private _dbService: DatabaseService;
+  constructor(dbService: DatabaseService) { 
+    this._dbService = dbService;
+  }
 
   ngOnInit(): void {
+
+    this.capacity = this._dbService.getNumberMembersCheckedIn();
+    
+
+    this.revenue = this._dbService.getTotalRevenue();
   }
 
 }
